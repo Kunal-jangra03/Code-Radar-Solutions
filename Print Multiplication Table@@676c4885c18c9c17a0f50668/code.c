d4bf06eae273d8ccb9eccf7b2458d62f5ca218b5
Kunal-jangra03 <kunal1211.becse24@chitkara.edu.in>
2025-03-11 16:03:05 +0530

    Solution to Print Multiplication Table

--- Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
+++ Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
@@ -2,11 +2,8 @@
 int main(){
     int n,i,j;
     scanf("%d",&n);
-    for(i=5;i<=10;i++){
-        for(j=0;j<=n*10;j=j+n){
-            printf("%d",j);
-        }
-        printf("\n");
+    for(i=1;i<=10;i++){
+        printf("%d \n",n*i);
     }
     return 0;
 
